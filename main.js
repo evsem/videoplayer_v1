73139c6dcc233a768aec284d2addb2e2d6039c57
evsem <frukwork@gmail.com>
2022-03-28 22:35:26 +0300

    V-2(play and pause func)

---
+++ main.js
@@ -0,0 +1,31 @@
+//Получаем переменные
+const video = document.querySelector('.video')
+const playBtn = document.querySelector('.controls__play')
+const pauseBtn = document.querySelector('.controls__pause')
+const stopBtn = document.querySelector('.controls__stop')
+const progress = document.querySelector('.progress')
+const time = document.querySelector('.controls__time')
+
+//Запуск и остановка видео
+function togglePlayVideoStatus() {
+  if (video.paused) {
+    video.play()
+  }
+}
+playBtn.addEventListener('click', togglePlayVideoStatus)
+function togglePauseVideoStatus() {
+  if (video.played) {
+    video.pause()
+  }
+}
+pauseBtn.addEventListener('click', togglePauseVideoStatus)
+
+//При клике на само видео происходит play и pause
+function playAndPauseOnClickVideo() {
+  if (video.paused) {
+    video.play()
+  } else {
+    video.pause()
+  }
+}
+video.addEventListener('click', playAndPauseOnClickVideo)
